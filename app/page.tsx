300e8a33697473e3e057f91684bd42d34a88a421
cpparnell <connorparnell2018@gmail.com>
2024-02-13 20:00:27 -0600

    better mobile formatting, still not perfect

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Badge from './components/Badge'
 import About from './components/About'
 import ExperienceTimeline from './components/ExperienceTimeline'
 import Education from './components/Education';
+import ContactForm from './components/ContactForm';
+import PersonalCard from './components/PersonalCard';
 
 export default function Home() {
   return (
@@ -14,8 +16,13 @@ export default function Home() {
           <></>
         </Head>
         <main className="flex flex-wrap min-h-screen flex-col items-center justify-between p-8">
-          <div className='min-w-[1200px] min-h-[500px] max-h-[1080pix]'>
+          <div className='hidden md:block'>
             <Badge />
+          </div>
+          <div className='min-w-[1200px] min-h-[500px] max-h-[1080pix]'>        
+            <div className='md:hidden pl-[400px] pr-[200px]'>
+              <PersonalCard />
+            </div>
             <About />
             <div className='pt-8'>
               <ExperienceTimeline />
@@ -24,11 +31,18 @@ export default function Home() {
               <Education />
             </div>
             <div className='pt-8 md:hidden w-full pl-[400px] pr-[200px]'>
+              <div className="shadow-lg block px-6 pt-6 pb-2 bg-white border border-gray-300 rounded-lg mb-8 md:hidden">
+                  <div>
+                      <ContactForm />
+                  </div>
+              </div>
+              <div className="shadow-lg block px-6 py-2 bg-white border border-gray-300 rounded-lg text-sm">
+                  <p>Like this website? <a className="text-gradient font-bold" href="https://github.com/cpparnell/nextjs-personal-website" >Check out the code here!</a></p>
+              </div>
             </div>
           </div>
         </main>
       </div>
     </body>
-
   )
 }
